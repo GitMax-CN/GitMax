22fa328870e00179ff98741d757d4a2cc41eb30e
Chen Li <yichen.li0830@gmail.com>
2016-12-13 11:46:53 -0800

    Improve addUser method to get more info from Github

--- serverless/users/addUser.js
+++ serverless/users/addUser.js
@@ -5,37 +5,61 @@
 'use strict';
 
 let AWS = require("aws-sdk");
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+});
+
 let docClient = new AWS.DynamoDB.DocumentClient();
+let request = require('request');
 
 module.exports = (event, context, callback) => {
-  // console.log("event.body", event.body);
   let data = JSON.parse(event.body);
+  console.log("data", JSON.stringify(data));
   if (!data.id || !data.token) callback(new Error("Data format error: id and token are required."));
   
-  let params = {
-    TableName:"test",
-    Item: {
-      ...data,
-      registeredAt: new Date().getTime(),
-    },
+  let options = {
+    url: "https://api.github.com/user/" + data.id + "?access_token=" + data.token,
+    method: "get",
+    headers: {
+      'User-Agent': "GitMax"
+    }
   };
   
-  docClient.put(params, function(err, data) {
-    if (err) {
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-      console.log("Added item:", JSON.stringify(data, null, 2));
+  request(options, function (error, response, body) {
+    if (error) callback(new Error("Can't get user's data from Github:", JSON.stringify(error)));
+    else {
+      let getData = JSON.parse(body);
+      // console.log("data", data);
+      // console.log("getData", getData);
+      let user = Object.assign({}, getData, {"token": data.token, "email": data.email, "id": data.id});
+  
+      Object.keys(user).forEach(key => {// Dela with empty attributes
+        if (user[key] === null || (typeof user[key] === "string" && user[key].length === 0)) user[key] = "N/A";
+      });
+      // console.log("user", user);
+      
+      let params = {
+        TableName: "test",
+        Item: user,
+      };
+
+      docClient.put(params, function (err, data) {
+        if (err) {
+          console.error("Unable to add item. Error JSON:", JSON.stringify(err));
+          callback(new Error(JSON.stringify(err)));
+        } else {
+          // console.log("Added item:", JSON.stringify(data));
+          const response = {
+            statusCode: 200,
+            headers: {"Access-Control-Allow-Origin": "*"},
+            body: JSON.stringify(data),
+          };
+          callback(null, response);
+        }
+      });
     }
   });
   
-  const response = {
-    statusCode: 200,
-    headers: { "Access-Control-Allow-Origin" : "*"},
-    body: JSON.stringify({
-      message: 'get event successfully!',
-    }),
-  };
   
-  callback(null, response);
-  // const data = event.body;
 };
