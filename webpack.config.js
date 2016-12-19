7a95521c9bd7c39beb5af09ce0bf767f908b3d74
Chen Li <yichen.li0830@gmail.com>
2016-12-19 15:16:49 -0800

    Configure the app to work with ant.design

--- webpack.config.js
+++ webpack.config.js
@@ -8,20 +8,26 @@ let APP_DIR = path.resolve(__dirname, 'client/src');
 // console.log("path.resolve(__dirname)", path.resolve(__dirname));
 // console.log("BUILD_DIR", BUILD_DIR);
 
+
 let config = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
-  module : {
-    loaders : [
+  module: {
+    loaders: [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel'
-      }
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel'
+      },
+      {test: /\.css/, loader: 'style-loader!css-loader'},
+      {test: /\.less$/, loader: 'style!css!less'},
     ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
   }
 };
 
