181c2b40425705ee12816727112e4d64047a3dbe
Chen Li <yichen.li0830@gmail.com>
2016-12-18 21:24:30 -0800

    Set up basic routing

---
+++ client/src/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+  render(){
+    return <div>
+      About Page
+    </div>
+  }
+}
+
+export default AboutPage;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import {render} from 'react-dom';
 import PageNotFound from './PageNotFound.jsx';
-import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router';
-
-class App extends React.Component {
-  render () {
-    return <div>
-      <p> Hello React! </p>
-      {this.props.children}
-    </div>
-  }
-}
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import App from './App.jsx';
+import Home from './Home.jsx';
+import AboutPage from './AboutPage.jsx';
 
 const router = (
-      <Router history={hashHistory}>
-        {/*<Router>*/}
+    <Router history={hashHistory}>
+      {/*<Router>*/}
       <Route path="/" component={App}>
-        {/*<IndexRoute component={AuthSignInNew} onEnter={verifyNotLogin}/>*/}
-        {/*<Route path="verify-email/:token" component={VerifyEmailTokenPage}/>*/}
-        <Route path="nn" component={PageNotFound}/>
-        <Route path="*" component={PageNotFound}/>
+        <IndexRoute component={Home}/>
+        <Route path="about" component={AboutPage}/>
       </Route>
+      <Route path="*" component={PageNotFound}/>
     </Router>
 );
 
