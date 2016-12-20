def8a9aca0510437aad9e6da06fbec7d8f05f825
Chen Li <yichen.li0830@gmail.com>
2016-12-19 22:39:53 -0800

    Update Content 2 component

--- client/src/Home/Content2.jsx
+++ client/src/Home/Content2.jsx
@@ -4,57 +4,56 @@ import TweenOne from 'rc-tween-one';
 import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
 
 class Content extends React.Component {
-
+  
   static propTypes = {
     id: React.PropTypes.string,
     className: React.PropTypes.string,
   };
-
+  
   static defaultProps = {
     className: 'content1',
   };
-
+  
   render() {
     return (
-      <div
-        {...this.props}
-        className="content-template-wrapper content-half-wrapper"
-      >
-        <OverPack
-          className={`content-template ${this.props.className}`}
-          hideProps={{ img: { reverse: true } }}
-          location={this.props.id}
+        <div
+            {...this.props}
+            className="content-template-wrapper content-half-wrapper"
         >
-          <QueueAnim
-            type="left"
-            className={`${this.props.className}-text`}
-            key="text"
-            leaveReverse
-            ease={['easeOutCubic', 'easeInCubic']}
+          <OverPack
+              className={`content-template ${this.props.className}`}
+              hideProps={{img: {reverse: true}}}
+              location={this.props.id}
           >
-            <h1
-              key="h1"
+            <QueueAnim
+                type="left"
+                className={`${this.props.className}-text`}
+                key="text"
+                leaveReverse
+                ease={['easeOutCubic', 'easeInCubic']}
             >
-              分布式中间件
-            </h1>
-            <p
-              key="p"
+              <h1
+                  key="h1"
+              >
+                找到认识的他们
+              </h1>
+              <p
+                  key="p"
+              >
+                社交网络上好友虽多，Github上却不认识几个？GitMax帮你在Github上寻找熟人。编程之路从此与他们结伴同行。
+              </p>
+            </QueueAnim>
+            <TweenOne
+                key="img"
+                animation={{x: '+=30', opacity: 0, type: 'from'}}
+                className={`${this.props.className}-img`}
             >
-              金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。
-            </p>
-          </QueueAnim>
-          <TweenOne
-            key="img"
-            animation={{ x: '+=30', opacity: 0, type: 'from' }}
-            className={`${this.props.className}-img`}
-          >
-            <span
-            >
-              <img width="100%" src="https://zos.alipayobjects.com/rmsportal/tvQTfCupGUFKSfQ.png" />
+            <span style={{width: '75%', right: '5%'}}>
+              <img width="100%" src="images/filmtocat.png"/>
             </span>
-          </TweenOne>
-        </OverPack>
-      </div>
+            </TweenOne>
+          </OverPack>
+        </div>
     );
   }
 }
