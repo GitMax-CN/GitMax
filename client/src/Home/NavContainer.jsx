bb881874b9139cf60a849796c6516cb65384c856
Chen Li <yichen.li0830@gmail.com>
2016-12-22 20:27:57 -0800

    Remove unnecessary logs

--- client/src/Home/NavContainer.jsx
+++ client/src/Home/NavContainer.jsx
@@ -3,8 +3,8 @@ import {connect} from 'react-redux';
 import Nav from './Nav';
 
 const mapStateToProps = (state, ownProps) => {
-  console.log("state.user", state.user);
-  console.log("ownProps", ownProps);
+  // console.log("state.user", state.user);
+  // console.log("ownProps", ownProps);
   return {
     user: state.user,
     ...ownProps
