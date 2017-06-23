62f9236f01654ef4f8be9662511dd285f0d1ba20
Chen Li <yichen.li0830@gmail.com>
2017-06-23 11:06:04 -0700

    Apply our categories on to this model and train the network with out dataset

--- neuralNetworkModel/useANN.js
+++ neuralNetworkModel/useANN.js
@@ -12,12 +12,12 @@ const dict = dictJSON;
 
 const classes_array = Object.keys(ANN_Classes);
 
-const test_history = 'The beginning of the Viking Age in the British Isles is, however, often given as 793.';
-const test_music = 'Baroque music is a style of Western art music composed from approximately 1600 to 1750';
-const test_bow_history = mimir.bow(test_history, dict);
-const test_bow_music = mimir.bow(test_music, dict);
+const test_intro = 'Good afternoon, this is chen calling from nomii';
+const test_benefits = 'With this product, you can significantly reduce coaching prep time for your sales team';
+const test_bow_intro = mimir.bow(test_intro, dict);
+const test_bow_benefits = mimir.bow(test_benefits, dict);
 
-let predict = net.run(test_bow_history);
-console.log(predict);
-console.log(classes_array[api.maxarg(predict)]);
-console.log(classes_array[api.maxarg(net.run(test_bow_music))]);
+console.log(net.run(test_bow_intro));
+console.log(classes_array[api.maxarg(net.run(test_bow_intro))]);
+console.log(net.run(test_bow_benefits));
+console.log(classes_array[api.maxarg(net.run(test_bow_benefits))]);
