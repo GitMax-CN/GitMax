5933c32beb3f5047976ff18dbb16a9a9dcb10af8
Chen Li <yichen.li0830@gmail.com>
2017-06-26 15:24:08 -0700

    Adding explanation modal

--- client/src/Components/RecordModal.js
+++ client/src/Components/RecordModal.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import {Modal} from 'antd';
-import {compose, withState} from 'recompose';
+import {Modal, Tag} from 'antd';
+import {compose, withState, withHandlers} from 'recompose';
 
 const RecordModal = (props) =>{
   console.log("RecordModal props", props);
   return <Modal
-      title="Basic Modal"
+      title="Read Me"
       visible={props.visible}
+      okText = "Ok"
+      cancelText="Cancel"
       onOk={()=>props.updateModal(false)}
-      //onCancel={()=>props.updateModal(false)}
+      onCancel={props.onCancel}
   >
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
+    Hi there,
+    <p>In order to try this demo, please make sure your mic is working.</p>
+    <p>This demo is trained with a very small sales call dataset (10 calls). Therefore, It only understand certain words and sentence structures.</p>
+    {"You can find examples by clicking the "}
+    <Tag color="green">
+      {"colored tags"}
+    </Tag>
+    {"in the right column"}
   </Modal>
 };
 
-export default RecordModal;
+export default compose(
+    withHandlers({
+      onCancel: props => () => {
+        props.updateModal(false);
+        props.stopRecording();
+      }
+    }),
+)(RecordModal);
 
 
 
